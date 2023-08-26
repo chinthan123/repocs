fb3828f002a4a655029f30da359a25833fc708ca
chinthan <chinthanshetty2005@gmail.com>
2023-08-26 12:12:50 +0530

    Initial commit

---
+++ python_progs/arith.py
@@ -0,0 +1,5 @@
+def add (n1,n2):
+    return(n1+n2)
+a=int(input("Enter a num1:"))
+b=int(input("Enter a num2:"))
+print("Sum=",add(a,b))
---
+++ readme.txt
@@ -0,0 +1,2 @@
+My name is chinthan
+vivekanada polytechnic 
